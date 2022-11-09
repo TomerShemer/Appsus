63622ab1c3f7aeb89bf97ddcae98e8af4cead913
barzaken <bar.zaken@boyar.org.il>
2022-11-09 23:52:10 +0200

    first day is over

--- apps/mail/cmps/email-filter.cmp.js
+++ apps/mail/cmps/email-filter.cmp.js
@@ -2,14 +2,20 @@ export default{
     template:`
     <div className="email-filter">
         <input @input="onFilter" className="filter-input" placeholder="Search email" type="text" v-model="filter.txt" />
+        <div className="filter-btns">
+            <button @click="onFilter('all')" className="filter-btn">All</button>
+            <button @click="onFilter('read')"  className="filter-btn">Read</button>
+            <button @click="onFilter('unread')"  className="filter-btn" >Unread</button>
+        </div>
     </div>`,
     data(){
         return{
-            filter:{txt:''}
+            filter:{txt:'',mode:''}
         }
     },
     methods:{
-        onFilter(){
+        onFilter(value){
+            if(value) this.filter.mode = value
             this.$emit('filtered',{...this.filter})
         }
     }
--- apps/mail/cmps/email-preview.cmp.js
+++ apps/mail/cmps/email-preview.cmp.js
@@ -7,10 +7,11 @@ export default {
         <button className="star-btn">{{getStarIcon}}</button>
         <h3>{{email.from}}</h3>
         <div className="email-info">
-            <h3>{{email.subject}}</h3>
-            <h3>{{email.body}}</h3>
+            <h3 class="bold">{{email.subject}}</h3>
+            <h3>{{getSummaryBody}}</h3>
         </div>
-        <h5>{{email.sentAt}}</h5>
+        <h5>{{getDate}}</h5>
+
     </div>
     <email-details :email="email" v-if="isOpen" />
     `,
@@ -19,19 +20,30 @@ export default {
             isOpen: false,
         }
     },
-    methods:{
-        togglePreview(){
+    methods: {
+        togglePreview() {
             this.isOpen = !this.isOpen
         },
 
     },
-    computed:{
-        getStarIcon(){
-            return this.email.isStar ? '⭐ ' : '&#9734'
-        }
+    computed: {
+        getStarIcon() {
+            return this.email.isStar ? '⭐ ' : '☆'
+        },
+        getDate() {
+            let date = new Date(this.email.sentAt)
+            let day = 1000 * 60 * 60 * 24
+            if (Date.now() - this.email.sentAt < day) return date.toLocaleTimeString()
+            return date.toLocaleDateString()
+        },
+        getSummaryBody() {
+            if (this.email.body.length > 30) return this.email.body
+            return this.email.body.substring(0, 50).trim() + '...'
+        },
+
     },
-    components:{
-        emailDetails
+    components: {
+        emailDetails,
     }
 
 
