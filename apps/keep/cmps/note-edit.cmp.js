1aef079f28361a9fb1dab50ea3dffd460248bfae
Tomer Shemer <tomershemer@gmail.com>
2022-11-10 14:04:33 +0200

    basic edit setup

--- apps/keep/cmps/note-edit.cmp.js
+++ apps/keep/cmps/note-edit.cmp.js
@@ -1,8 +1,10 @@
+
 export default {
     props: [],
     template: `
         <section className="note-edit">
             <h1>Edit your note</h1>
+            <component :is=""></component>
         </section>
         `,
     data() {
