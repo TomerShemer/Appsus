9ef3bb95365aafca3b0590c0361ab8b896715b91
barzaken <bar.zaken@boyar.org.il>
2022-11-12 19:22:26 +0200

    about

--- views/app-about.cmp.js
+++ views/app-about.cmp.js
@@ -1,7 +1,23 @@
 export default {
 	template: `
         <section class="about-page">
-            <h1>About us</h1>
+           <div className="project-info">
+               <h1>On this porject</h1>
+               <h2>This project was made for sprint3 on Coding Academy Fullstack Bootcamp!</h2>
+               <h2>Cloning Google Gmail and Keep Apps</h2>
+           </div>
+            <div className="profile-container">
+                <div className="profile1">
+                    <img src="https://img.a.transfermarkt.technology/portrait/big/3139-1459504284.jpg?lm=1" alt="" />
+                    <h2>Bar Zaken</h2>
+                    <router-link to="/mail"><h3>Gmail App</h3></router-link> 
+                </div>
+                <div className="profile2">
+                    <img src="https://img.a.transfermarkt.technology/portrait/big/3139-1459504284.jpg?lm=1" alt="" />
+                    <h2>Tomer Shemer</h2>
+                    <router-link to="/notes"><h3>Notes App</h3></router-link> 
+                </div>
+            </div>
         </section>
     `
 }
--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -5,7 +5,8 @@ export default {
             <div class="hero-image">
                 <div class="hero-text">
                     <h1>Appsus</h1>
-                    <p>Made By</p>
+                    <h2>Made By</h2>
+                    <h2>Bar Zaken & Tomer Shemer</h2>
                     <div className="links">
                         <router-link to="/notes"><button>Notes</button></router-link> |
                         <router-link to="/mail"><button>Mail</button></router-link>
