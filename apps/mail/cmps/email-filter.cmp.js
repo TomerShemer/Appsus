c195031947e50a65a390a1f6cce38f5416012838
barzaken <bar.zaken@boyar.org.il>
2022-11-12 21:18:15 +0200

    change input filter type

--- apps/mail/cmps/email-filter.cmp.js
+++ apps/mail/cmps/email-filter.cmp.js
@@ -2,7 +2,7 @@
 export default {
     template: `
     <div className="email-filter">
-        <input @input="onFilter" className="filter-input" placeholder="Search in mail" type="text" v-model="filter.txt" />
+        <input @input="onFilter" className="filter-input" placeholder="Search in mail" type="search" v-model="filter.txt" />
         <div className="filter-btns">
             <button @click="onFilter('all')" :class="{active : filter.mode ==='all'}" class="filter-btn">All</button>
             <button @click="onFilter('read')" :class="{active : filter.mode ==='read'}" class="filter-btn">Read</button>
