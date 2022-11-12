73edd59d69519eb049431959d2d73dc3fedd9d31
barzaken <bar.zaken@boyar.org.il>
2022-11-12 18:00:03 +0200

    books in

--- routes.js
+++ routes.js
@@ -5,6 +5,8 @@ import mailPage from './apps/mail/email-app.cmp.js'
 import emailList from './apps/mail/cmps/email-list.cmp.js'
 import emailDetails from './apps/mail/pages/email-details.cmp.js'
 import bookApp from './apps/books/js/views/books-app.cmp.js'
+import bookAdd from './apps/books/js/views/book-add.cmp.js'
+import bookDetails from './apps/books/js/views/book-details.cmp.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -26,22 +28,33 @@ const routerOptions = {
 		{
 			path: '/mail',
 			component: mailPage,
-			children:[
+			children: [
 				{
-					path:'',
-					component : emailList,
-	
+					path: '',
+					component: emailList,
+
 				},
 				{
 					path: '/:id',
-					component : emailDetails,
-					props:true,
+					component: emailDetails,
+					props: true,
 				},
 			]
 		},
 		{
 			path: '/book',
 			component: bookApp,
+		},
+		{
+			path: '/add-book',
+			component: bookAdd,
+			props: true,
+
+		},
+		{
+			path: '/book/:id',
+			component: bookDetails,
+			props: true,
 		},
 
 
